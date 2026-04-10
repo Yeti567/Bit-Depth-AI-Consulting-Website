5f6ce3f96c2a29d958d02b025eebc44de213e5c7
Yeti567 <blake@yetidigitalservices.com>
2026-04-09 19:16:40 -0700

    Add Google Analytics tracking code to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -88,6 +88,21 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1KBYH51CSC"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-1KBYH51CSC');
+            `,
+          }}
+        />
+      </head>
       <body className={`${barlow.variable} font-sans antialiased`}>
         {children}
       </body>
